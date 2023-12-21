10af88f99deeaa8fed3110c2161d8f1e141fff21
cheesysamira <samiram18@nycstudents.net>
2023-12-20 22:33:44 -0500

    TH6 Completed

--- src/Town.java
+++ src/Town.java
@@ -104,6 +104,7 @@ public class Town {
      */
     public void enterShop(String choice) {
         shop.enter(hunter, choice);
+        printMessage = "You left the shop";
     }
 
     public void huntForTreasure() {
